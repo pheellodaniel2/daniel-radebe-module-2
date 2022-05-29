a6ea06b0fc68c1a63e88b9988c9c8d74e114dc70
pheellodaniel2 <88340125+pheellodaniel2@users.noreply.github.com>
2022-05-29 16:58:06 +0200

    Update code2(lists).dart

--- code2(lists).dart
+++ code2(lists).dart
@@ -1,1 +1,29 @@
+void main() {
 
+ //Declaring and initializing List of  winning apps of the MTN Business App of the Year Awards since 2012
+ var winningApps=<String>[];//Enter the names of the apps and  their winning year
+//(a)Sorting  apps by name
+ winningApps.sort();
+  // Print the apps each on a new line 
+  for(var app in winningApps){
+ print(app);
+  }
+//(b)Printng the winning app of 2017 and the winning app of 2018
+  
+	printWinner(winningApps,"2017");
+  printWinner(winningApps,"2018");
+  
+//(c)Printing the total number of apps from the array
+	print(winningApps.length);
+  
+}
+
+void printWinner(List<String> liss,String year){
+  
+  for(var app in liss){
+    if(app.contains(year)){
+      print(app);
+    }
+  }
+    
+}
